5a0d26dfa5fb0c2cf8ef75f6c3414d1d4497c124
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-12-11 00:33:20 +0600

    use thread local storage

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import threading
 
-from time import sleep
+data = threading.local()
 
 
-def test():
-    while True:
-        print("test")
-        sleep(1)
+def get_data():
+    thread_name = threading.current_thread().name
+    print(thread_name, data.value)
+    
 
 
-threading.Timer(10, test).start()
+def set_value_data_in_thread_1():
+    data.value = 111
+    get_data()
 
 
-while True:
-    print(1)
-    sleep(2)
+def set_value_data_in_thread_2():
+    data.value = 222
+    get_data()
+
+
+threading.Thread(target=set_value_data_in_thread_1).start()
+threading.Thread(target=set_value_data_in_thread_2).start()
