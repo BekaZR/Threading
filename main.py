693e88395167146737056ae6da4ba8a13b1f2234
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-12-10 22:39:05 +0600

    use two thread

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from datetime import datetime
 
 
 def get_date(date):
-    thr_name = threading.current_thread().name
-    print(f"{thr_name} -- {date}")
-    sleep(5)
+    while True:
+        thr_name = threading.current_thread().name
+        print(f"{thr_name} -- {date}")
+        sleep(2)
 
 thr = threading.Thread(target=get_date, args=(str(datetime.now()),), name="thr-1").start()
 
+
+for i in range(10):
+    print(i)
+    sleep(2)
